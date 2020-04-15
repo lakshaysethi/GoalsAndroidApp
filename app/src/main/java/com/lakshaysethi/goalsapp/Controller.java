454cb93bcc5c8e2106860df0457e4a6d3b41b768
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-16 01:00:08 +1200

    Update Controller.java

--- app/src/main/java/com/lakshaysethi/goalsapp/Controller.java
+++ app/src/main/java/com/lakshaysethi/goalsapp/Controller.java
@@ -5,16 +5,14 @@ import com.lakshaysethi.goalsapp.importantObjects.Goal;
 import java.util.ArrayList;
 
 public class Controller {
-    public ArrayList<Goal> getLatestGoalsList() {
-        ArrayList<Goal>  al =new ArrayList<Goal>();
-        al.add(new Goal("Money","$1,000,000"));
 
-       
-        al.add(new Goal("health","$1,000,000"));
-        al.add(new Goal("my space","$1,000,000"));
-        al.add(new Goal("Flow State","$1,000,000"));
-        al.add(new Goal("Lucid dreaming","$1,000,000"));
+    public static ArrayList<Goal>  gal=new ArrayList<Goal>();
 
-        return al;
+    Controller(){
+
+
     }
+
+
+
 }
