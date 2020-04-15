803375e435a417a5f198fb74351dd4b1ebbafedf
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-16 00:59:40 +1200

    goal page

---
+++ app/src/main/java/com/lakshaysethi/goalsapp/activities/GoalPage.java
@@ -0,0 +1,16 @@
+package com.lakshaysethi.goalsapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lakshaysethi.goalsapp.R;
+
+public class GoalPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_page);
+    }
+}
