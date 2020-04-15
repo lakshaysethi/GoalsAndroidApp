a286272a63a67a40aa01914198602a0532e1eb5e
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-16 01:00:04 +1200

    make work on plan

---
+++ app/src/main/java/com/lakshaysethi/goalsapp/activities/WorkOnPlan.java
@@ -0,0 +1,15 @@
+package com.lakshaysethi.goalsapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lakshaysethi.goalsapp.R;
+public class WorkOnPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_on_plan);
+    }
+}
