51cb01b76cf45d0ab9768dfce70b3e6e60f811a4
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-16 00:59:05 +1200

    work on task

---
+++ app/src/main/java/com/lakshaysethi/goalsapp/activities/WorkOnTask.java
@@ -0,0 +1,14 @@
+package com.lakshaysethi.goalsapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.lakshaysethi.goalsapp.R;
+public class WorkOnTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_on_task);
+    }
+}
